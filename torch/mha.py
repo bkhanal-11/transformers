e835bb4e08d8fb1c5fecf30c948bcbbef2bbd017
Bishwash Khanal <lionelbishwash11@gmail.com>
2023-02-26 22:42:51 +0545

    modified mhs to separate scaled dot product

--- torch/mha.py
+++ torch/mha.py
@@ -1,7 +1,31 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import math
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, dropout_rate, **kwargs):
+        super(ScaledDotProductAttention, self).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, queries, keys, values, mask=None):
+        # Calculate dot products between the query and the key
+        matmul_qk = torch.matmul(queries, keys.transpose(-2, -1))
+
+        dk = keys.size()[-1]
+        scaled_attention_logits = matmul_qk / (dk ** 0.5)
+
+        if mask is not None:
+            scaled_attention_logits += (1. - mask) * -1e9 
+        
+        # Apply softmax function to obtain attention weights
+        attention_weights = F.softmax(scaled_attention_logits, dim=-1)
+
+        # Apply dropout to the attention weights
+        attention_weights_dropout = self.dropout(attention_weights)
+
+        output = torch.matmul(attention_weights_dropout, values)
+
+        return output, attention_weights
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, d_model, dropout_rate=0.1):
@@ -9,39 +33,32 @@ class MultiHeadAttention(nn.Module):
 
         assert d_model % num_heads == 0
 
+        self.attention = ScaledDotProductAttention(dropout_rate)
+
         self.num_heads = num_heads
-        self.d_head = d_model // num_heads
-        self.query_linear = nn.Linear(d_model, d_model)
-        self.key_linear = nn.Linear(d_model, d_model)
-        self.value_linear = nn.Linear(d_model, d_model)
-        self.dropout = nn.Dropout(dropout_rate)
-        self.output_linear = nn.Linear(d_model, d_model)
+        self.d_head = d_model // num_heads # key_dim = value_dim = d_head
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model)
 
     def forward(self, query, key, value, mask=None):
         batch_size = query.size(0)
 
         # Apply linear transformations to the inputs
-        query = self.query_linear(query).view(batch_size, -1, self.num_heads, self.d_head).transpose(1, 2)  # (batch_size, num_heads, query_seq_len, d_head)
-        key = self.key_linear(key).view(batch_size, -1, self.num_heads, self.d_head).transpose(1, 2)  # (batch_size, num_heads, key_seq_len, d_head)
-        value = self.value_linear(value).view(batch_size, -1, self.num_heads, self.d_head).transpose(1, 2)  # (batch_size, num_heads, value_seq_len, d_head)
+        query = self.W_q(query)
+        key = self.W_k(key)
+        value = self.W_v(value)
 
-        # Calculate dot products between the query and the key
-        scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(self.d_head)  # (batch_size, num_heads, query_seq_len, key_seq_len)
+        # Reshape the inputs for appropriate attention calculation
+        query = query.view(batch_size, -1, self.num_heads, self.d_head).transpose(1, 2)  # (batch_size, num_heads, query_seq_len, d_head)
+        key = key.view(batch_size, -1, self.num_heads, self.d_head).transpose(1, 2)  # (batch_size, num_heads, key_seq_len, d_head)
+        value = value.view(batch_size, -1, self.num_heads, self.d_head).transpose(1, 2)  # (batch_size, num_heads, value_seq_len, d_head)
 
-        if mask is not None:
-            scores += (1. - mask) * -1e9 
-
-        # Apply softmax function to obtain attention weights
-        attention_weights = F.softmax(scores, dim=-1)  # (batch_size, num_heads, query_seq_len, key_seq_len)
-
-        # Apply dropout to the attention weights
-        attention_weights = self.dropout(attention_weights)
-
-        # Multiply the attention weights by the value
-        context = torch.matmul(attention_weights, value)  # (batch_size, num_heads, query_seq_len, d_head)
+        context, attention_weights = self.attention(query, key, value, mask)
 
         # Concatenate the output of the heads and apply a linear transformation
         context = context.transpose(1, 2).contiguous().view(batch_size, -1, self.num_heads * self.d_head)  # (batch_size, query_seq_len, d_model)
-        output = self.output_linear(context)  # (batch_size, query_seq_len, d_model)
+        output = self.W_o(context)  # (batch_size, query_seq_len, d_model)
 
         return output, attention_weights
