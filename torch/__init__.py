a1a0436864b24413d2c35ad40dea350ee1a27b92
Bishwash Khanal <lionelbishwash11@gmail.com>
2023-02-26 23:38:02 +0545

    added __init__.py

--- torch/__init__.py
+++ torch/__init__.py
@@ -1,4 +1,4 @@
 from .transformer import Transformer
 from .utils import create_padding_mask, create_look_ahead_mask
 
-__all__ = ['TransformerModel', 'create_padding_mask', 'create_look_ahead_mask']
+__all__ = ['Transformer', 'create_padding_mask', 'create_look_ahead_mask']
